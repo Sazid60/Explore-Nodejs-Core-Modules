e0e973f3637984cfe0a10224b453ca466fc677ad
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2025-05-31 14:53:02 +0600

    Add : 13-9 Creating and Reading A ToDo using Postman

--- Readme.md
+++ Readme.md
@@ -780,4 +780,68 @@ if (req.url === "/todos" && req.method === "GET") {
 
 - now we will use file system instead of fake data
 
-##
+## 13-9 Creating and Reading A ToDo using Postman
+
+- lets use our file system for database.
+
+- we will create indentation while doing stringify, i mean this will give space an something
+
+```js
+fs.writeFileSync(filePath, JSON.stringify(parsedAllTodos, null, 2), {
+  encoding: "utf-8",
+});
+res.end(JSON.stringify({ title, body, createdAt }, null, 2));
+```
+
+- it will format the data
+
+![alt text](image-10.png)
+
+- Final File System
+
+```js
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+
+const filePath = path.join(__dirname, "./db/todo.json");
+
+const server = http.createServer((req, res) => {
+  // get all todos
+  if (req.url === "/todos" && req.method === "GET") {
+    const data = fs.readFileSync(filePath, { encoding: "utf-8" });
+    res.writeHead(200, {
+      "content-type": "application/json",
+    });
+    res.end(data);
+  }
+  // post a todo
+  else if (req.url === "/todos/create-todo" && req.method === "POST") {
+    let data = "";
+
+    req.on("data", (chunk) => {
+      data = data + chunk;
+    });
+
+    req.on("end", () => {
+      const { title, body } = JSON.parse(data);
+      console.log({ title, body });
+
+      const createdAt = new Date().toLocaleString();
+      const allTodos = fs.readFileSync(filePath, { encoding: "utf-8" });
+      const parsedAllTodos = JSON.parse(allTodos);
+      parsedAllTodos.push({ title, body, createdAt });
+      fs.writeFileSync(filePath, JSON.stringify(parsedAllTodos, null, 2), {
+        encoding: "utf-8",
+      });
+      res.end(JSON.stringify({ title, body, createdAt }, null, 2));
+    });
+  } else {
+    res.end("Route Not Found");
+  }
+});
+
+server.listen(5000, "127.0.0.1", () => {
+  console.log("Server Is Listening");
+});
+```
--- codes/ToDo-APP/server.js
+++ codes/ToDo-APP/server.js
@@ -1,35 +1,40 @@
 const http = require("http")
+const path = require("path")
+const fs = require("fs")
 
+const filePath = path.join(__dirname, "./db/todo.json")
 
-const data = [
-    {
-        "title": "Morning Routines",
-        "body": "Boost your day with simple habits.",
-        "createdAt": "2025-05-30T08:45:00Z"
-    },
-    {
-        "title": "AI in Life",
-        "body": "How AI is changing everything.",
-        "createdAt": "2025-05-29T17:20:00Z"
-    }
-]
 
 const server = http.createServer((req, res) => {
-    // console.log({ req, res })
 
+    // get all todos
     if (req.url === "/todos" && req.method === "GET") {
+        const data = fs.readFileSync(filePath, { encoding: "utf-8" })
         res.writeHead(200, {
-            // "content-type": "text/plain",
-            // "content-type": "application/json",
-            "content-type": "text/html",
-            // "email": "ph@gmail.com"
+            "content-type": "application/json",
         })
-        // res.end(JSON.stringify(data))
-
-        res.end(`<h1> Hello I am HTmL</h1>`)
+        res.end(data)
     }
+    // post a todo
     else if (req.url === "/todos/create-todo" && req.method === "POST") {
-        res.end("Todo Created")
+        let data = ""
+
+        req.on("data", (chunk) => {
+            data = data + chunk
+        })
+
+        req.on("end", () => {
+            const { title, body } = JSON.parse(data)
+            console.log({ title, body })
+
+            const createdAt = new Date().toLocaleString()
+            const allTodos = fs.readFileSync(filePath, { encoding: "utf-8" })
+            const parsedAllTodos = JSON.parse(allTodos)
+            parsedAllTodos.push({ title, body, createdAt })
+            fs.writeFileSync(filePath, JSON.stringify(parsedAllTodos, null, 2), { encoding: "utf-8" })
+            res.end(JSON.stringify({ title, body, createdAt }, null, 2))
+        })
+
     } else {
         res.end("Route Not Found")
     }
